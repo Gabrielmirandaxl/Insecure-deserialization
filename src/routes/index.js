544782cf21b440647945459ba2f4e061c07a99db
Gabriel Miranda <gabrielmirandaxl109@gmail.com>
2024-09-25 06:34:51 -0300

    projeto offsec

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 const controller = require('../controllers/index')
 
-router.get("/home", controller.welcome)
+router.get("/welcome", controller.welcome)
 router.post("/register", controller.register)
 router.post("/login", controller.login)
 
